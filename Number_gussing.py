cbbe3f39965a30be6de0036d401847ed6513c81a
YasH4556 <yg616576@gmail.com>
2026-01-31 18:02:39 +0530

    Improved ig

--- Number_gussing.py
+++ Number_gussing.py
@@ -2,18 +2,16 @@ import numpy as np
 import sys
 
 # Getting input from user for range
-a = input("Enter the lower limit of range:")
-if (a ==""):
-    a = 1
-else:
-    a = int(a)
-b = input("Enter the upper limit of range:")
-if (b==""):
-    b = 100
-else:
-    b = int(b)
-    if (a >= b ):
-        sys.exit("Please enter a valid input")
+try:
+    a = int(input("Enter the lower limit of range:"))
+except ValueError:
+    sys.exit("Please Enter a valid input")
+try:
+    b = int(input("Enter the upper limit of range:"))
+except ValueError:
+    sys.exit("Please Enter a valid input")
+if (a >= b ):
+    sys.exit("Please enter a valid input")
 
 # Generating random number
 random = np.random.randint(a,b)
@@ -23,21 +21,22 @@ print(random)
 #actual code for number guessing
 guess = None
 while (guess != random ):
-    guess = input(f"Guess a number from {a} to {b} :")
-    if (guess == "" or b <= int(guess) <= a):
-        print("Please enter a valid guess")
-    else:
-        guess = int(guess)
-        if (guess == random ):
+    try:
+        guess = int(input(f"Guess a number from {a} to {b} :"))
+    except ValueError:
+         continue
+    if (guess > b or guess <a):
+        print("Enter a valid input")
+        continue
+    elif (guess == random ):
             print("Your guess was correct")
-            break
-        else :
-            percent = (abs(guess - random) - 1)/(b-a+1)
-            if (percent <= 0.1):
-                print("Soo close! You were almost their")
-            elif(percent <=0.5):
-                print("Close! You can do better")
-            elif (percent <= 0.75):
-                print("Not close! Try again")
-            else:
-                print("Way off! You need to improve a lot")
+    else :
+        percent = (abs(guess - random) - 1)/(b-a+1)
+        if (percent <= 0.1):
+            print("Soo close! You were almost their")
+        elif(percent <=0.5):
+            print("Close! You can do better")
+        elif (percent <= 0.75):
+            print("Not close! Try again")
+        else:
+            print("Way off! You need to improve a lot")
